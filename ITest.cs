d508ef8817edc0959bc3748d6f58826a8ab3ff60
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-12-31 22:22:07 +0600

    project done

---
+++ ITest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration_Testing
+{
+    public class ITest
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("/usr/local/bin/chromedriver_linux64");
+        }
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://elp.duetbd.org/login/index.php";
+            IWebElement element = driver.FindElement(By.Name("username"));
+            element.SendKeys("170042065");
+            IWebElement password = driver.FindElement(By.Name("password"));
+            password.SendKeys("Abc.1234");
+            driver.FindElement(By.Id("loginbtn")).Click();
+            String at = driver.Title;
+            Console.WriteLine(at);
+
+            String et = "E-Learning Platform | DUET, Gazipur: Log in to the site";
+            if (at == et)
+            {
+                Console.WriteLine("Test Successful");
+            }
+            else
+            {
+                Console.WriteLine("Unsuccessful");
+            }
+        }
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ SeleniumTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class ELPTestCase
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver("/usr/local/bin/chromedriver_linux64");
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheELPTestCaseTest()
+        {
+            driver.Navigate().GoToUrl("https://elp.duetbd.org/");
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("username")).Clear();
+            driver.FindElement(By.Id("username")).SendKeys("170042065");
+            driver.FindElement(By.Id("password")).Clear();
+            driver.FindElement(By.Id("password")).SendKeys("Abc.1234");
+            driver.FindElement(By.Id("loginbtn")).Click();
+            driver.FindElement(By.Id("action-menu-toggle-1")).Click();
+            driver.FindElement(By.LinkText("Profile")).Click();
+            driver.FindElement(By.Id("action-menu-toggle-1")).Click();
+            driver.FindElement(By.Id("actionmenuaction-1")).Click();
+            driver.FindElement(By.Id("action-menu-toggle-1")).Click();
+            driver.FindElement(By.Id("actionmenuaction-6")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
